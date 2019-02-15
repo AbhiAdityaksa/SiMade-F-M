990f98bcfc7793c6e45280c6a6ec205bb4e68a1c
abhiadityaksa <abhiadityaksa@gmail.com>
2019-02-15 11:09:40 +0800

    update filepicker

--- app/src/main/java/com/example/simadeui/MainActivity.java
+++ app/src/main/java/com/example/simadeui/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.simadeui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv_regis1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        tv_regis1 = (TextView) findViewById(R.id.tv_regis1);
+
+        tv_regis1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,Register2Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/simadeui/Register2Activity.java
+++ app/src/main/java/com/example/simadeui/Register2Activity.java
@@ -1,11 +1,16 @@
 package com.example.simadeui;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.nbsp.materialfilepicker.MaterialFilePicker;
 import com.nbsp.materialfilepicker.ui.FilePickerActivity;
@@ -24,8 +29,14 @@ public class Register2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register2);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1001);
+        }
+
         btn_kk_1 = (Button) findViewById(R.id.btn_u_kk);
         tv_kk_1 = (TextView) findViewById(R.id.tv_u_kk);
+        btn_ktp_2 = (Button) findViewById(R.id.btn_u_ktp);
+        tv_ktp_2 = (TextView) findViewById(R.id.tv_u_ktp);
 
         btn_kk_1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,12 +44,22 @@ public class Register2Activity extends AppCompatActivity {
                 new MaterialFilePicker()
                         .withActivity(Register2Activity.this)
                         .withRequestCode(1000)
-                        .withFilter(Pattern.compile(".*\\.jpg$")) // Filtering files and directories by file name using regexp
-                        .withFilterDirectories(true) // Set directories filterable (false by default)
                         .withHiddenFiles(true) // Show hidden files and folders
                         .start();
             }
         });
+
+        btn_ktp_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new MaterialFilePicker()
+                        .withActivity(Register2Activity.this)
+                        .withRequestCode(1000)
+                        .withHiddenFiles(true) // Show hidden files and folders
+                        .start();
+            }
+        });
+
 
     }
 
@@ -50,6 +71,21 @@ public class Register2Activity extends AppCompatActivity {
             String filePath = data.getStringExtra(FilePickerActivity.RESULT_FILE_PATH);
             // Do anything with file
             tv_kk_1.setText(filePath);
+            tv_ktp_2.setText(filePath);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 1001:{
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(this,"Sukses!",Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(this,"Gagal!",Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
         }
     }
 }
