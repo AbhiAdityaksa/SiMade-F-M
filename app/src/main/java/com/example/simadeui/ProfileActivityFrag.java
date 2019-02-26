dccbf20c25bf49f9acfad0ecfccd1d60456b89bd
PrabaBa <prabahridayami97@gmail.com>
2019-02-26 18:25:31 +0800

    edit profil

--- app/src/main/java/com/example/simadeui/ProfileActivityFrag.java
+++ app/src/main/java/com/example/simadeui/ProfileActivityFrag.java
@@ -1,18 +1,45 @@
 package com.example.simadeui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class ProfileActivityFrag extends Fragment {
-
+public class ProfileActivityFrag extends Fragment{
+    View v;
+    TextView tv_det_profile;
+    TextView tv_chang_pass;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
-        return inflater.inflate(R.layout.activity_profile_frag, container, false);
+        v  = inflater.inflate(R.layout.activity_profile_frag, container, false);
+
+        tv_det_profile = v.findViewById(R.id.tv_det_profile);
+        tv_chang_pass = v.findViewById(R.id.tv_change_pass);
+
+        tv_det_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(),DetProfileActivity.class);
+                startActivity(intent);
+                getActivity().finish();
+            }
+        });
+
+        tv_chang_pass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(),GantiPassActivity.class);
+                startActivity(intent);
+                getActivity().finish();
+            }
+        });
+        return v;
 
     }
+
 }
