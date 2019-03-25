3afb17fa138ae1af99c0f9f5d305d248bc7e128a
abhiadityaksa <abhiadityaksa@gmail.com>
2019-03-25 10:55:19 +0800

    update bottonav di notifactivity

--- app/src/main/java/notifActivity/NotifActivity.java
+++ app/src/main/java/notifActivity/NotifActivity.java
@@ -1,11 +1,14 @@
 package notifActivity;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -15,6 +18,8 @@ import com.example.simadeui.HomeActivityFrag;
 import com.example.simadeui.R;
 
 import Auth.DetProfileActivity;
+import Auth.MainActivity;
+import userReport.LaporanActivityFrag;
 import utama.UtamaActivity;
 
 public class NotifActivity extends AppCompatActivity {
@@ -27,6 +32,7 @@ public class NotifActivity extends AppCompatActivity {
         setContentView(R.layout.activity_notif);
 
         btn_back = findViewById(R.id.btn_back);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
 
         btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,6 +40,25 @@ public class NotifActivity extends AppCompatActivity {
                 Intent intent = new Intent(NotifActivity.this,UtamaActivity.class);
                 startActivity(intent);
                 finish();
+            }
+        });
+
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.navigation_home:
+                        Intent a = new Intent(MainActivity.this,UtamaActivity.class);
+                        startActivity(a);
+                        break;
+                    case R.id.navigation_dashboard:
+                        Intent b = new Intent(MainActivity.this, LaporanActivityFrag.class);
+                        startActivity(b);
+                        break;
+                    case R.id.navigation_notifications:
+                        break;
+                }
+                return false;
             }
         });
 
